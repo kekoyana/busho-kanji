481fdfff024e135ec24a091679d031942649ac49
kekoyana <kekoyana0@gmail.com>
2020-07-29 16:21:05 +0900

    クリップボードへのコピー作成

--- code.js
+++ code.js
@@ -1,15 +1,28 @@
 const name = document.getElementById("inputName");
+const cText = document.getElementById("copyText");
 const result = document.getElementById("result");
 
 function searchByText(text) {
   const options = {
-    includeScore: true
+    includeScore: true,
+    keys: ['kanji', 'kana', 'romaji'],
   }
   const fuse = new Fuse(List, options);
   return fuse.search(text);
 }
 
+function copyClip(text) {
+  cText.value = text;
+  cText.select();
+  document.execCommand('copy');
+}
+
 name.addEventListener('change', (event) => {
-  search_result = searchByText(event.target.value)[0];
-  if (search_result) result.value = search_result.item;
+  search_result = searchByText(event.target.value);
+  if (search_result.length == 0) return;
+  var html = "";
+  search_result.forEach(function (res) {
+    html += `<button id="text" onClick="copyClip('${res.item.kanji}')">${res.item.kanji}</button>`;
+  });
+  result.innerHTML = html;
 });
