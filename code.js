027d2d801ba0b5107d3786016d7b0a3d36087953
kekoyana <kekoyana0@gmail.com>
2020-07-29 19:21:24 +0900

    武将入力の説明を追記

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const name = document.getElementById("inputName");
-const cText = document.getElementById("copyText");
+const rText = document.getElementById("resultText");
 const result = document.getElementById("result");
 
 function searchByText(text) {
@@ -13,14 +13,21 @@ function searchByText(text) {
 }
 
 function copyClip(text) {
+  rText.innerHTML = '<input type=text id="copyText" size=8 /> コピーしました';
+  var cText = document.getElementById("copyText");
   cText.value = text;
   cText.select();
   document.execCommand('copy');
 }
 
-name.addEventListener('change', (event) => {
-  search_result = searchByText(event.target.value);
+name.addEventListener('keyup', (event) => {
+  rText.innerHTML = "";
+  if (event.target.value.length < 3) return;
+
+  var search_result = searchByText(event.target.value);
+
   if (search_result.length == 0) return;
+
   var html = "";
   search_result.forEach(function (res) {
     html += `<button id="text" onClick="copyClip('${res.item.kanji}')">${res.item.kanji}</button>`;
